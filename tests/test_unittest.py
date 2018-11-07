99af1ce94b22602904ce2a4b2a0e98b57ed2270f
Brad Buchanan <bradley.c.buchanan@gmail.com>
2018-11-07 15:38:59 -0800

    Inject blocks

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -1,8 +1,124 @@
+import os
 import unittest
 from appium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select
 from time import sleep
+
+# Six dancers of each character, cycling through the repeat animations
+HEAVY_LOAD = """
+<xml>
+ <block type="Dancelab_whenSetup" movable="false">
+  <statement name="DO">
+   <block type="Dancelab_makeNewDanceSpriteGroup">
+    <title name="N">6</title>
+    <title name="COSTUME">"ALIEN"</title>
+    <title name="LAYOUT">"circle"</title>
+    <next>
+     <block type="Dancelab_makeNewDanceSpriteGroup">
+      <title name="N">6</title>
+      <title name="COSTUME">"BEAR"</title>
+      <title name="LAYOUT">"circle"</title>
+      <next>
+       <block type="Dancelab_makeNewDanceSpriteGroup">
+        <title name="N">6</title>
+        <title name="COSTUME">"CAT"</title>
+        <title name="LAYOUT">"circle"</title>
+        <next>
+         <block type="Dancelab_makeNewDanceSpriteGroup">
+          <title name="N">6</title>
+          <title name="COSTUME">"DOG"</title>
+          <title name="LAYOUT">"circle"</title>
+          <next>
+           <block type="Dancelab_makeNewDanceSpriteGroup">
+            <title name="N">6</title>
+            <title name="COSTUME">"DUCK"</title>
+            <title name="LAYOUT">"circle"</title>
+            <next>
+             <block type="Dancelab_makeNewDanceSpriteGroup">
+              <title name="N">6</title>
+              <title name="COSTUME">"FROG"</title>
+              <title name="LAYOUT">"circle"</title>
+              <next>
+               <block type="Dancelab_makeNewDanceSpriteGroup">
+                <title name="N">6</title>
+                <title name="COSTUME">"MOOSE"</title>
+                <title name="LAYOUT">"circle"</title>
+                <next>
+                 <block type="Dancelab_makeNewDanceSpriteGroup">
+                  <title name="N">6</title>
+                  <title name="COSTUME">"PINEAPPLE"</title>
+                  <title name="LAYOUT">"circle"</title>
+                  <next>
+                   <block type="Dancelab_makeNewDanceSpriteGroup">
+                    <title name="N">6</title>
+                    <title name="COSTUME">"ROBOT"</title>
+                    <title name="LAYOUT">"circle"</title>
+                    <next>
+                     <block type="Dancelab_makeNewDanceSpriteGroup">
+                      <title name="N">6</title>
+                      <title name="COSTUME">"SHARK"
+                      </title>
+                      <title name="LAYOUT">"circle"
+                      </title>
+                      <next>
+                       <block type="Dancelab_makeNewDanceSpriteGroup">
+                        <title name="N">6
+                        </title>
+                        <title name="COSTUME">
+                         "UNICORN"
+                        </title>
+                        <title name="LAYOUT">
+                         "circle"
+                        </title>
+                       </block>
+                      </next>
+                     </block>
+                    </next>
+                   </block>
+                  </next>
+                 </block>
+                </next>
+               </block>
+              </next>
+             </block>
+            </next>
+           </block>
+          </next>
+         </block>
+        </next>
+       </block>
+      </next>
+     </block>
+    </next>
+   </block>
+  </statement>
+ </block>
+ <block type="Dancelab_everySeconds">
+  <title name="N">1</title>
+  <title name="UNIT">"measures"</title>
+  <statement name="DO">
+   <block type="Dancelab_setBackgroundEffect">
+    <title name="EFFECT">['disco', 'color_cycle', 'rainbow', 'text',
+     'diamonds','splatter','swirl','spiral'][randomInt(0,7)]
+    </title>
+    <next>
+     <block type="Dancelab_setForegroundEffect">
+      <title name="EFFECT">['spotlight', 'rain', 'raining_tacos'][randomInt(0,2)]</title>
+      <next>
+       <block type="Dancelab_changeMoveEachLR">
+        <title name="GROUP">sprites</title>
+        <title name="MOVE">"next"</title>
+        <title name="DIR">-1</title>
+       </block>
+      </next>
+     </block>
+    </next>
+   </block>
+  </statement>
+ </block>
+</xml>
+"""
 
 
 class DanceStressTest(unittest.TestCase):
@@ -19,6 +135,7 @@ class DanceStressTest(unittest.TestCase):
         # self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', {})
         self.driver.orientation = "LANDSCAPE"
+        self.screenshot_folder = os.getenv('SCREENSHOT_PATH', '/tmp')
 
     def tearDown(self):
         self.driver.quit()
@@ -30,16 +147,27 @@ class DanceStressTest(unittest.TestCase):
         # Enter age and dismiss dialog, which causes a page reload
         self.bypassAgeDialog()
 
-        # Run basic dance party for 30 seconds
+        # Wait for the page to load again
         self.waitToSee('runButton')
+
+        self.screenshot('loaded')
+
+        # svgFlyoutGroup
+        # Then I open the topmost blockly category "Brushes"
+        #   And I drag block matching selector "#draw-color" to block matching selector "#when_run"
+        self.setBlocks(HEAVY_LOAD)
+        self.screenshot('blocks_loaded')
+
+        # Run basic dance party for 30 seconds
         self.clickRun()
         sleep(30)
         self.clickReset()
         sleep(1)
         self.assertTrue(self.isNotRunning())
 
-        # TODO: Load all characters
-        # TODO: Play all dances
+        self.screenshot('test_over')
+
+        # TODO: Play short burst dances
         # TODO: Run/reset several times to catch memory leaks
         # TODO: Force out-of-memory to see what happens
 
@@ -50,6 +178,14 @@ class DanceStressTest(unittest.TestCase):
         submit = self.driver.find_element_by_id('uitest-submit-age')
         submit.click()
         sleep(3)  # Enough time for reload to start
+
+    def setBlocks(self, block_xml):
+        self.driver.execute_script("""
+            var blocksXml = arguments[0];
+            Blockly.mainBlockSpace.clear();
+            var arrangedBlocksXml = __TestInterface.arrangeBlockPosition(blocksXml, {});
+            __TestInterface.loadBlocks(arrangedBlocksXml);
+        """, block_xml)
 
     def clickRun(self):
         el = self.driver.find_element_by_id('runButton')
@@ -76,6 +212,9 @@ class DanceStressTest(unittest.TestCase):
                     raise
                 else:
                     sleep(1)  # Wait one second then check again
+
+    def screenshot(self, name):
+        self.driver.save_screenshot(self.screenshot_folder + '/' + name + '.png')
 
 
 # Start of script
